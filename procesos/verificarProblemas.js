5695ba3473b52caaa481e64ba3615b565b43ccc1
Ruben Requena <rubnrequena@gmail.com>
2021-02-04 10:41:10 +0100

    #5 en periodo de prueba

--- procesos/verificarProblemas.js
+++ procesos/verificarProblemas.js
@@ -1,4 +1,4 @@
-console.log('Verificando errores en la pagina');
+
 if (document.body.innerHTML.indexOf('Se ha producido un error en el sistema') > -1) {
   var myAudio = new Audio(chrome.runtime.getURL(`assets/error.mp3`));
   myAudio.play().then(() => {
@@ -6,6 +6,12 @@ if (document.body.innerHTML.indexOf('Se ha producido un error en el sistema') > 
   })
 }
 if (document.body.innerHTML.indexOf('Su sesión ha caducado por permanecer demasiado tiempo inactiva') > -1) {
+  var myAudio = new Audio(chrome.runtime.getURL(`assets/error.mp3`));
+  myAudio.play().then(() => {
+    window.location.href = 'https://sede.administracionespublicas.gob.es/icpplus/index.html';
+  })
+}
+if (document.body.innerHTML.indexOf('No ha superado la validación de seguridad asociada al Captcha del servicio') > -1) {
   var myAudio = new Audio(chrome.runtime.getURL(`assets/error.mp3`));
   myAudio.play().then(() => {
     window.location.href = 'https://sede.administracionespublicas.gob.es/icpplus/index.html';
