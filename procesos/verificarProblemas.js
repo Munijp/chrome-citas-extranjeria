9454ec9b9c4cc98cc7e6fbc92a2b27535bd4083e
Ruben Requena <rubnrequena@gmail.com>
2021-02-08 16:47:49 +0100

    fix #7

--- procesos/verificarProblemas.js
+++ procesos/verificarProblemas.js
@@ -2,7 +2,7 @@
 if (document.body.innerHTML.indexOf('Se ha producido un error en el sistema') > -1) {
   var myAudio = new Audio(chrome.runtime.getURL(`assets/error.mp3`));
   myAudio.play().then(() => {
-    window.location.reload()
+    window.location.href = 'https://sede.administracionespublicas.gob.es/icpplus/index.html';
   })
 }
 if (document.body.innerHTML.indexOf('Su sesión ha caducado por permanecer demasiado tiempo inactiva') > -1) {
